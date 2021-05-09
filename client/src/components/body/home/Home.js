37fcce9d692619d52dc84f6cfd5f0d92cc810a50
unknown <milzaza123@gmail.com>
2021-05-09 22:42:59 +0700

    make it better

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hamster Hub ยินดีต้อนรับน้า</h2>
+            <p>
+                ใครอยากเข้าไปในคุยกันในเพจได้ที่นี้เลย
+            </p>
+            <a href="https://www.facebook.com/HamsterHubThailand" target="_blank" 
+            rel="noopener noreferrer">Facebook</a>
+
+            
+        </div>
+    )
+}
+
+export default Home
